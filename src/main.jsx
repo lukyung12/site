e7be3e442dcf805952c70390b000a2cef04557cd
lukyung12 <mwakalukgosy2@gmail.com>
2026-02-22 19:08:38 +0300

    Fix build

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter basename="/site">
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 )
