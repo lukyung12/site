2cca7d98eae78dc929e0a48f0adc030cbf85a422
lukyung12 <mwakalukgosy2@gmail.com>
2026-02-22 18:56:15 +0300

    Fix router base path from /site/ to /

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Cathy Cleans</h1>
+      <p>Welcome to our cleaning service!</p>
+    </div>
+  )
+}
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: "/site/"
-})
+export default {
+  base: "/"
+}
