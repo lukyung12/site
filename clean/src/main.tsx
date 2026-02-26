eca588ae9142424a532708e52593bb6622c4b1c3
lukyung12 <mwakalukgosy2@gmail.com>
2026-02-26 18:03:22 +0300

    rebuild with basename

--- clean/src/main.tsx
+++ clean/src/main.tsx
@@ -8,7 +8,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/site">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/contact" element={<Contact />} />
