497a6f9c924dd7266c42c17d55fc6e56fda8d477
lukyung12 <mwakalukgosy2@gmail.com>
2026-03-06 10:16:16 +0300

    change on route to contacts

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import Contact from './Contact'
 import './index.css'
